142724473b8f7692e72468fb506576e4990347cc
Aakash Solanki <tech2aks@gmail.com>
2024-05-06 01:49:59 +0530

    add control from action button

--- app/src/main/java/com/example/bleexample/receivers/BLEConnectionReceiver.kt
+++ app/src/main/java/com/example/bleexample/receivers/BLEConnectionReceiver.kt
@@ -4,12 +4,20 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import com.example.bleexample.models.PacketManager
+import com.example.bleexample.models.RC
 
 class BLEConnectionReceiver: BroadcastReceiver() {
     override fun onReceive(p0: Context?, p1: Intent?) {
         Log.d("BLEConnectionReceiver", "onReceive")
         if(p1!=null){
             Log.d("BLEConnectionReceiver", p1.action.toString())
+            when(p1.action.toString()){
+                "NEXT" -> {PacketManager.sendRemotePacket(RC.NEXT)}
+                "PREVIOUS" -> {PacketManager.sendRemotePacket(RC.PREV)}
+                "PAUSE" -> {PacketManager.sendRemotePacket(RC.PLAY)}
+                "PLAY" -> {PacketManager.sendRemotePacket(RC.PLAY)}
+            }
 
         }
         else{
